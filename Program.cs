c6b755a539aa1641519ef195b8b2767e7fd37325
salvatore <tore0145@gmail.com>
2022-03-31 18:42:25 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("BENVENUTI");
+Console.WriteLine("GENNAIO");
+Console.WriteLine("FEBBARIO");
+Console.WriteLine("MARZO");
+Console.WriteLine("APLRILE"); 
+Console.WriteLine("MAGGIO"); 
+Console.WriteLine("GIUGNO");
+Console.WriteLine("LUGLIO");
+Console.WriteLine("AGOSTO");
+Console.WriteLine("SETTEMBRE");
+Console.WriteLine("OTTOBRE");
+Console.WriteLine("NOVEBRE");
+Console.WriteLine("DICEMBRE");
+Console.WriteLine("QUAL'è IL VOSTRO MESE PREFERITO?");
+Console.ReadLine();
+Console.WriteLine("grazie per aver risposto");
